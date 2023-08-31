2d05d4212e0c4a049aca578a8b2aeed32ce6cf96
Manelito <emanuelwork1@gmail.com>
2023-08-31 13:29:39 -0300

    feat: create account service login method

--- src/app/account/shared/account.service.ts
+++ src/app/account/shared/account.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '@/environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
+  private api_url = environment.api;
+  private headers: HttpHeaders;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    const token = localStorage.getItem('token');
+    this.headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    });
+  }
+
+  public login(params: LoginParams): Observable<any> {
+    return this.http.post(`${this.api_url}/auth/login`, params, { headers: this.headers });
+  }
+}
+
+export interface LoginParams {
+  login: string;
+  password: string;
 }
