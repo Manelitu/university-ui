03b5f4e13b605250f64d37a28b2c54ac6c7536b9
Manelito <emanuelwork1@gmail.com>
2023-08-31 18:31:14 -0300

    feat: create interceptor using headers and import it

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,8 +1,18 @@
 import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { AuthInterceptor } from '../account/shared/auth.interceptor';
+
 
 @NgModule({
   imports: [CommonModule],
-  exports: []
+  exports: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    }
+  ]
 })
 export class PagesModule { }
