ef670edbf397eab387bd4c1c1f96989f959f918b
Manelito <emanuelwork1@gmail.com>
2023-08-31 20:24:52 -0300

    fix: adding components on module

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -7,13 +7,17 @@ import { ToastModule } from 'primeng/toast';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
+import { LogoutComponent } from './logout/logout.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
     LoginComponent,
     HomeComponent,
     AuthenticationComponent,
-    SidebarComponent
+    SidebarComponent,
+    LogoutComponent,
+    LayoutComponent
   ],
   imports: [CommonModule, ToastModule, FormsModule, ReactiveFormsModule, RouterModule.forRoot([])],
   exports: [LoginComponent]
