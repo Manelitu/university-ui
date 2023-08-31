37a83ecdcbaa79afb429fe6384d6fe3e1b7b583b
Manelito <emanuelwork1@gmail.com>
2023-08-31 20:24:40 -0300

    fix: adding correct component

--- src/app/pages/route.module.ts
+++ src/app/pages/route.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { HomeComponent } from '../layout/home/home.component';
-import { SidebarComponent } from '../components/sidebar/sidebar.component';
 import { DisciplinesComponent } from './disciplines/disciplines.component';
 import { CoursesComponent } from './courses/courses.component';
 import { PeriodsComponent } from './periods/periods.component';
+import { LayoutComponent } from '../components/layout/layout.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginPageComponent },
-  { path: 'app', component: SidebarComponent,  children: [
+  { path: 'app', component: LayoutComponent,  children: [
     { path: '', component: HomeComponent },
     { path: 'disciplines', component: DisciplinesComponent },
     { path: 'courses', component: CoursesComponent },
