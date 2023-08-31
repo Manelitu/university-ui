160bd1c1893409cc9ef6a905c17733747dee1de4
Manelito <emanuelwork1@gmail.com>
2023-08-31 18:57:39 -0300

    feat: create home childrens of routes

--- src/app/pages/route.module.ts
+++ src/app/pages/route.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { HomeComponent } from '../layout/home/home.component';
+import { SidebarComponent } from '../components/sidebar/sidebar.component';
+import { DisciplinesComponent } from './disciplines/disciplines.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginPageComponent }
+  { path: 'login', component: LoginPageComponent },
+  { path: 'home', component: SidebarComponent,  children: [
+    { path: '', component: HomeComponent },
+    { path: 'disciplines', component: DisciplinesComponent }
+  ] }
 ]
 
 @NgModule({
