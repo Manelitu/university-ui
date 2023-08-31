5871e22a0360a1555377013aba4eb464245d10c2
Manelito <emanuelwork1@gmail.com>
2023-08-31 20:24:27 -0300

    refactor: removing unecessary code

--- src/app/account/shared/account.service.ts
+++ src/app/account/shared/account.service.ts
@@ -2,30 +2,24 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '@/environments/environment';
 import { Observable } from 'rxjs';
+import { removeStorage } from '../helpers/localstorage.utils';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
   private api_url = environment.api;
-  private headers: HttpHeaders;
 
-  constructor(private http: HttpClient) {
-    const token = localStorage.getItem('token');
-    if (token) {
-      this.headers = new HttpHeaders({
-        Authorization: `Bearer ${token}`,
-        'Content-Type': 'application/json'
-      });
-    } else {
-      this.headers = new HttpHeaders({
-        'Content-Type': 'application/json'
-      });
-    }
-  }
+  constructor(private http: HttpClient, private route: Router) {}
 
   public login(params: LoginParams): Observable<any> {
-    return this.http.post(`${this.api_url}/auth/login`, params, { headers: this.headers });
+    return this.http.post(`${this.api_url}/auth/login`, params);
+  }
+
+  public logout(): void {
+    removeStorage('token');
+    this.route.navigate(['/login']);
   }
 }
 
