1146eb704c091cd3fa9ec411b2c9e4c214a240cb
Manelito <emanuelwork1@gmail.com>
2023-09-01 23:11:55 -0300

    feat: create sidebar component layout

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthTokenService } from '../../account/shared/auth.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent {
-  constructor() {}
+export class SidebarComponent implements OnInit {
+  constructor(private auth: AuthTokenService) {}
+  public isCoordinator: boolean = false;
+  public isAdmin: boolean = false;
+  public isUser: boolean = false;
+
+  ngOnInit(): void {
+    this.isCoordinator = this.auth.isCoordinator();
+    this.isAdmin = this.auth.isAdmin();
+    this.isUser = this.auth.isUser();
+  }
 }
